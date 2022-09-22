745edfe75d3a379f2824c95f200ed39f995d32a1
kaluchii <mr.kaluchii@mail.ru>
2022-09-23 03:58:15 +0600

    Add ru locale

---
+++ resources/lang/ru/filament-navigation.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'attributes' => [
+        'external-link' => 'Прямая ссылка',
+        'url' => 'URL',
+        'target' => 'Как открывать',
+        'name' => 'Заголовок',
+        'items' => 'Ссылки',
+        'handle' => 'Ключ',
+        'created_at' => 'Created at',
+        'updated_at' => 'Updated at',
+     ],
+
+    'select-options' => [
+        'same-tab' => 'На этой же вкладке',
+        'new-tab' => 'На новой вкладке'
+    ],
+
+    'items' => [
+        'empty' => 'Пусто',
+        'add-item' => 'Добавить ссылку',
+        'add-child' => 'Добавить ссылку нижнего уровня',
+        'move-up' => 'Поднять',
+        'move-down' => 'Опустить',
+        'indent' => 'Остступить',
+        'dedent' => 'Задвинуть',
+        'remove' => 'Удалить'
+    ],
+
+    'items-modal' => [
+        'title' => 'Ссылка',
+        'label' => 'Текст ссылки',
+        'type' => 'Тип',
+        'btn' => 'Сохранить',
+    ],
+];
